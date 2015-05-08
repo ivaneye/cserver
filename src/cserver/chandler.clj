af42111e9fec65dc9e250a84c3c21e1ad5f286ca
ivanpig <ivan.pig@foxmail.com>
2015-05-08 14:45:59 +0800

    clojure handler response

--- src/cserver/chandler.clj
+++ src/cserver/chandler.clj
@@ -1,6 +1,16 @@
 (ns cserver.chandler
-  (:import (io.netty.channel SimpleChannelInboundHandler ChannelHandlerContext)))
+  (:import (io.netty.channel SimpleChannelInboundHandler ChannelHandlerContext ChannelFutureListener)
+           (io.netty.handler.codec.http HttpRequest LastHttpContent)
+           (io.netty.buffer Unpooled)))
 
+(defn do-request [ctx msg]
+  (.write ctx msg))
+
+(defn do-content [ctx msg]
+  (if (instance? LastHttpContent msg)
+    (.addListener (.writeAndFlush ctx Unpooled/EMPTY_BUFFER) ChannelFutureListener/CLOSE))
+  (println "Content = " msg)
+  (println "---------------------------------------"))
 
 (defn handler
   "创建SimpleChannelInboundHandler子类，处理请求"
@@ -20,5 +30,5 @@
       (.printStackTrace cause)
       (.close ctx))
     (channelRead0 [^ChannelHandlerContext ctx msg]
-      (println "msg = " msg))
+      (if (instance? HttpRequest msg) (do-request ctx msg) (do-content ctx msg)))
     ))
